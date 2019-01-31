51fe1114acf58fc676e0c51dec3cfda704c9058c
Billgo <cocobill@vip.qq.com>
2019-01-31 15:18:12 +0800

    :art: update server

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.json({"message": "Building a RESTful CRUD API with Node.js, Express/Koa and MongoDB."});
+});
+
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
